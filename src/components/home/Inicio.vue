15aae1076cc31069eec25e9340b651cc0324b94d
Samuel <samuka16mf@gmail.com>
2021-07-17 13:58:57 -0300

    .

--- src/components/home/Inicio.vue
+++ src/components/home/Inicio.vue
@@ -5,12 +5,34 @@
 </template>
 
 <script>
+import { getCookie } from "../../utilidades/cookie";
+import axios from "axios"
+
 export default {
   data() {
-    return {
+    return {};
+  },
+  async beforeCreate() {
+    console.log("Carregando pagina de inicio..");
+    console.log("Checando se o usuario tem uma sessão valida");
+
+    let sessaoUsuario = getCookie("sessaoID", document.cookie);
+
+    if (sessaoUsuario.length != 0) {
+      console.log("Usuario tem uma sessão! Buscando ela no servidor..");
+      console.log(sessaoUsuario);
+
+      let resposta = await axios.post()
+
+      
+    } else {
+      console.log(
+        "Usuario não possui sessão. Redirecionando a pagina de login..."
+      );
+      this.$router.push("./login")
     }
-  }
-}
+  },
+};
 </script>
 
 <style>
